f7c568e4c116938688adb085ce8704a327a18cc6
lastoyster <kadampratiksha56@gmail.com>
2024-03-21 14:58:07 +0530

    first commit

---
+++ lib/screen/demo_screen.dart
@@ -0,0 +1,19 @@
+﻿import 'widget/animated_bottom_bar.dart';
+import 'package:flutter/material.dart';
+
+class DemoScreen extends StatelessWidget {
+  const DemoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: SizedBox(height: 90, child: AnimatedBottomAppBar()),
+      body: Center(
+        child: Text(
+          "Demo Screen",
+        ),
+      ),
+    );
+  }
+}
